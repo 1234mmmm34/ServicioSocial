c8eb1bdab0c3abf4946c062f159aacc8a27b9d7b
DELL <DELL@DESKTOP-SREQODJ>
2023-11-17 03:21:32 -0600

    Modificacion

---
+++ ServicioSocial/Clases/Carreras.cs
@@ -0,0 +1,30 @@
+﻿namespace ServicioSocial.Clases
+{
+    public class Carreras
+    {
+        public class Zona
+        {
+            public int zona { get; set; }
+            public string? nombre { get; set; }
+        
+        }
+
+        public class Facultad
+        {
+
+            public int id_facultad { get; set; }
+            public int id_zona { get; set; }
+            public string? nombre { get; set; }
+
+        }
+
+        public class Carrera
+        {
+
+            public int id_carrera { get; set; }
+            public int id_facultad { get; set; }
+            public string? nombre { get; set; }
+
+        }
+    }
+}
