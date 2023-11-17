1f5bba639583d74cdf0777740556146d739bf40d
DELL <DELL@DESKTOP-SREQODJ>
2023-11-17 03:12:16 -0600

    Fichas tecnicas

--- ServicioSocial/Clases/FichasTecnicas.cs
+++ ServicioSocial/Clases/FichasTecnicas.cs
@@ -32,7 +32,7 @@
             public int alumnos_aprobados { get; }
         }
 
-        public class docentes
+        public class Docentes
         {
             public int id_carrera { get; set; }
             public int tiempo_completo { get; set; }
@@ -47,5 +47,52 @@
             public int doctorado { get; set; }
 
         }
+
+        public class Matricula
+        {
+            public int id_carrera { get; set; }
+            public string? generacion { get; set; }
+            public int hombre_nuevo { get; set; }
+            public int mujer_nuevo { get; set; }
+            public int hombre_reingreso { get; set; }
+            public int mujer_reingreso { get; set; }
+
+        }
+
+        public class Programa_academico
+        {
+            public int id_carrera { get; set; }
+            public string? nombre { get; set; }
+            public string? documento_creacion { get; set; }
+            public string? numero_rvoe { get; set; }
+            public string? institucion_rvoe { get; set; }
+            public string? mision { get; set; }
+            public string? vision { get; set; }
+            public string? objetivos_estrategicos { get; set; }
+
+        }
+
+        public class Rendimiento_escolar
+        {
+            public int id_carrera { get; set; }
+            public string generacion { get; set; }
+            public int estudiantes { get; set; }
+            public int desercion { get; set; }
+            public int reprobacion { get; set; }
+            public int titulados { get; set; }
+
+        }
+
+        public class Responsables
+        {
+            public int id_responsable { get; set; }
+            public int id_carrera { get; set; }
+            public string? nombre { get; set; }
+            public string? cargo { get; set; }
+            public string? correo { get; set; }
+            public string? telefono { get; set; }
+
+        }
+
     }
 }
